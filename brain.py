8a96544be93b1fb356e98a5327f3c714d35b19e3
Abhay Karthik <abhaykarthik123@gmail.com>
2024-04-05 23:09:49 +0530

    brain of RogieAI

--- brain.py
+++ brain.py
@@ -1,1 +1,15 @@
 #brain of the virtual assistant 
+from typing import List, Tuple
+import re
+from langchain.docstore.document import Document
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores.faiss import FAISS
+import faiss
+
+
+def parse_text(text: str, filename: str) -> List[str]:
+    text = re.sub(r"(\w+)-\n(\w+)", r"\1\2", text)
+    text = re.sub(r"(?<!\n\s)\n(?!\s\n)", " ", text.strip())
+    text = re.sub(r"\n\s*\n", "\n\n", text)
+    return [text]
