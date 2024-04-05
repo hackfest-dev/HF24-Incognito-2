6faa128373e2f63134547ba2e3496975f38f4dda
PavithraCA <141633686+PavithraCA@users.noreply.github.com>
2024-04-05 20:46:29 +0530

    Update Youtube.py

--- pages/Youtube.py
+++ pages/Youtube.py
@@ -8,3 +8,12 @@ from zipfile import ZipFile
 
 st.markdown('# 📝 **Transcriber App**')
 bar = st.progress(0)
+
+# 2. Retrieving audio file from YouTube video
+def get_yt(URL):
+    video = YouTube(URL)
+    yt = video.streams.get_audio_only()
+    yt.download()
+
+    #st.info('2. Audio file has been retrieved from YouTube video')
+    bar.progress(10)
