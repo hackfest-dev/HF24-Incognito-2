9ce1733c866c11ebbbf531f353e33684d6a4edce
PavithraCA <141633686+PavithraCA@users.noreply.github.com>
2024-04-05 21:57:11 +0530

    Update Youtube.py

--- pages/Youtube.py
+++ pages/Youtube.py
@@ -17,3 +17,47 @@ def get_yt(URL):
 
     #st.info('2. Audio file has been retrieved from YouTube video')
     bar.progress(10)
+
+    # 3. Upload YouTube audio file to AssemblyAI
+def transcribe_yt():
+
+    current_dir = os.getcwd()
+
+    for file in os.listdir(current_dir):
+        if file.endswith(".mp4"):
+            mp4_file = os.path.join(current_dir, file)
+            #print(mp4_file)
+    filename = mp4_file
+    bar.progress(20)
+
+    def read_file(filename, chunk_size=5242880):
+        with open(filename, 'rb') as _file:
+            while True:
+                data = _file.read(chunk_size)
+                if not data:
+                    break
+                yield data
+    headers = {'authorization': api_key}
+    response = requests.post('https://api.assemblyai.com/v2/upload',
+                            headers=headers,
+                            data=read_file(filename))
+    audio_url = response.json()['upload_url']
+    #st.info('3. YouTube audio file has been uploaded to AssemblyAI')
+    bar.progress(30)
+
+    # 4. Transcribe uploaded audio file
+    endpoint = "https://api.assemblyai.com/v2/transcript"
+
+    json = {
+    "audio_url": audio_url
+    }
+
+    headers = {
+        "authorization": api_key,
+        "content-type": "application/json"
+    }
+
+    transcript_input_response = requests.post(endpoint, json=json, headers=headers)
+
+    #st.info('4. Transcribing uploaded file')
+    bar.progress(40)
