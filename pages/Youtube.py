784f8929ddc6aa616fcc762b638e9dcdc2714aeb
PavithraCA <141633686+PavithraCA@users.noreply.github.com>
2024-04-06 01:10:21 +0530

    Update Youtube.py

--- pages/Youtube.py
+++ pages/Youtube.py
@@ -106,3 +106,32 @@ def transcribe_yt():
     zip_file.write('yt.txt')
     zip_file.write('yt.srt')
     zip_file.close()
+
+    # app
+# 1. Read API from text file
+api_key = st.secrets['api_key']
+
+#st.info('1. API is read ...')
+st.warning('Awaiting URL input in the sidebar.')
+
+
+# Sidebar
+st.sidebar.header('Input parameter')
+
+
+with st.sidebar.form(key='my_form'):
+	URL = st.text_input('Enter URL of YouTube video:')
+	submit_button = st.form_submit_button(label='Go')
+
+# Run custom functions if URL is entered 
+if submit_button:
+    get_yt(URL)
+    transcribe_yt()
+
+    with open("transcription.zip", "rb") as zip_download:
+        btn = st.download_button(
+            label="Download ZIP",
+            data=zip_download,
+            file_name="transcription.zip",
+            mime="application/zip"
+        )
